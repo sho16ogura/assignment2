82e3de9122847eb0ded08f006ecf4a5eeb99d516
sho16ogura <69571783+sho16ogura@users.noreply.github.com>
2023-04-01 04:03:07 +0000

    Sat, Apr 1, 2023, 4:03 AM +00:00

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,5 +1,25 @@
 public class Assignment2{
     public boolean isAnagram(String aString, String bString){
-        char[] aArr=aString.toArray();
+        if(aString.length() != bString.length()){
+            return false;
+        }
+
+        char[] aArr=aString.toCharArray();
+        char[] bArr=bString.toCharArray();
+
+        for(int i=0; i<aArr.length; i++){
+            for(int j=0; j<aArr.length; j++){
+                if(aArr[i]==bArr[j]){
+                    bArr[j]='\\';
+                    break;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isAnagram("fried","fired"));
     }
 }
